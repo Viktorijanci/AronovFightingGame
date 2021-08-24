93e518c2137da1e7b8146e4a7f4b12d6a354e3a9
todayisyesterday <todayisy0@gmail.com>
2021-08-24 23:15:34 +0200

    test

---
+++ index.js
@@ -0,0 +1,99 @@
+import * as THREE from 'https://unpkg.com/three@0.119.1/build/three.module.js';
+import * as Game from './math.js';
+
+document.getElementById("startGame").addEventListener("click", function onEvent(event){
+  document.getElementById("startGame").style.display="none";
+  start();
+});
+
+function start(){
+  //Create the scene
+  let scene = new THREE.Scene();
+
+  //Create the camera
+  let camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  document.getElementById("gameRender").appendChild( renderer.domElement );
+
+  //Create the plane and add it to the scene
+  let geometry = new THREE.BoxGeometry(80,1,10);
+  const material = new THREE.MeshToonMaterial( {color: 0x00ff00} );
+  let cube = new THREE.Mesh( geometry, material );
+  scene.add(cube);
+
+  // White directional light at half intensity shining from the top.
+  let directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+  scene.add(directionalLight);
+
+  //vectors
+  const left = new THREE.Vector3(-1,0,0);
+  const right = new THREE.Vector3(1,0,0);
+
+  //Player models
+  let player1box = new THREE.BoxGeometry(5,3,1);
+  const player1material = new THREE.MeshToonMaterial({color:0xff0000});
+  let player1cube = new THREE.Mesh(player1box,player1material);
+  scene.add(player1cube);
+
+  let player2box = new THREE.BoxGeometry(5,3,1);
+  const player2material = new THREE.MeshToonMaterial({color:0x0000ff});
+  let player2cube = new THREE.Mesh(player2box,player2material);
+  scene.add(player2cube);
+
+  //attack direction
+  const attack1 = new THREE.Raycaster();
+  const attack2 = new THREE.Raycaster();
+  attack1.far=5;
+  attack2.far=5;
+
+  //scene setup
+  camera.position.y = 10;
+
+  cube.position.y = 3;
+  cube.position.z = -30;
+
+  player1cube.position.y =5;
+  player1cube.position.z=-30;
+  player1cube.position.x = -30;
+
+  player2cube.position.y =5;
+  player2cube.position.z =-30;
+  player2cube.position.x =30;
+
+  let player1 = new Game.Player(player1cube,attack1);
+  let player2 = new Game.Player(player2cube,attack2);
+  let playerArr = [player1,player2];
+
+  let num = 0, map={};
+  document.addEventListener("keyup", event => {
+    map[event.key]=false;
+  });
+  document.addEventListener("keydown", event => {
+    map[event.key]=true;
+  });
+
+  let counter1 = 0;
+  let counter2 = 0;
+  function animate() {
+    requestAnimationFrame(animate);
+    Game.mapPlayer1(map,player1);
+    Game.mapPlayer2(map,player2);
+    if(player1.attacking===true){
+      player1.cooldown=true;
+      counter1=23;
+    }
+    if(player2.attacking===true){
+      player2.cooldown=true;
+      counter2=23;
+    }
+    Game.resolveGravity(playerArr);
+    Game.resolveAttacking(playerArr,[left,right],cooldown);
+    // counter = counter<23 ? counter+=1 : 0;
+    // console.log(counter);
+    // cooldown = counter===23 ? true : false;
+    // console.log(cooldown);
+    renderer.render(scene, camera);
+  } animate();
+}
---
+++ math.js
@@ -0,0 +1,123 @@
+function mapPlayer1(map,player){
+  if(map.a){
+    player.cube.position.x-=1;
+    player.orient="left";
+  }
+  if(map.d){
+    player.cube.position.x+=1;
+    player.orient="right";
+  }
+  if(map.w){
+    player.jump();
+  }
+  if(map.e){
+    player.attack();
+    player.attackType="light";
+  }
+  if(map.r){
+    player.attack();
+    player.attackType="heavy";
+  }
+}
+
+function mapPlayer2(map,player){
+  if(map.j){
+    player.cube.position.x-=1;
+    player.orient="left";
+  }
+  if(map.l){
+    player.cube.position.x+=1;
+    player.orient="right";
+  }
+  if(map.i){
+    player.jump();
+  }
+  if(map.o){
+    player.attack();
+    player.attackType="light";
+  }
+  if(map.p){
+    player.attack();
+    player.attackType="heavy";
+  }
+}
+
+function resolveGravity(playerArr){
+  playerArr.forEach((item, i) => {
+    if(item.jumping && item.cube.position.y<10){
+      item.cube.position.y+=0.5;
+      return;
+    }
+    if(item.cube.position.y===10){
+      item.jumping=false;
+      item.descending=true;
+    }
+    if(item.descending && item.cube.position.y>4){
+      item.cube.position.y-=0.5;
+    }
+    if(item.cube.position.y===5){
+      item.descending=false;
+    }
+  });
+}
+
+function resolveAttacking(playerArr,sides,cooldown){
+  playerArr.forEach((item, i) => {
+    if(item.attacking && cooldown){
+      const side = player.orient==="left" ? sides[0] : sides[1];
+      const victim = i===0 ? 1 : 0;
+      item.attackBox.set(item.cube.position,side);
+      if(item.attackBox.intersectObject(playerArr[victim].cube).length!==0){
+        let audio = new Audio("./Recording.m4a");
+        audio.play();
+      };
+      item.attacking=false;
+      cooldown=false;
+    }else{
+      console.log("rip");
+    }
+  });
+}
+
+function resolveShielding(){
+
+}
+
+class Player {
+  constructor(cube,attackBox) {
+    //model
+    this.cube=cube;
+    this.attackBox=attackBox;
+
+    //status
+    this.stunned=false;
+
+    //movement
+    this.jumping=false;
+    this.descending=false;
+    this.orient="right";
+
+    //attacks
+    this.attacking=false;
+    this.cooldown=0;
+    this.attackType=null;
+    this.shielding=false;
+  }
+  jump(){
+    if(!this.stunned && !(this.descending && !this.jumping)){
+      this.jumping=true;
+    }
+  }
+  attack(){
+    if(!this.stunned && !this.attacking && !this.shielding){
+      this.attacking=true;
+    }
+  }
+  shield(){
+    if(!this.stunned && !this.attacking && !this.shielding){
+      this.shielding=true;
+    }
+  }
+}
+
+export {mapPlayer1,mapPlayer2,resolveGravity,resolveAttacking,Player};
